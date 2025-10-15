ca2a1f883b5898cf44f406a8636b08a3042c214a
unknown <kaido.kasuk@voco.ee>
2025-10-15 15:19:41 +0300

    1tund

---
+++ index.js
@@ -0,0 +1,20 @@
+// // console.log("hello");
+// // console.log("I like pizza");
+
+// // window.alert("this is an alert");
+// // window.alert("I love pizza");
+// // // this is an comment cntrl k+c cntrl k+
+
+// document.getElementById("myh1").textContent = "Hello";
+// document.getElementById("p").textContent = "i love ng";
+
+let x;
+x = 100;
+console.log(x);
+let age = 25;
+console.log(age);
+let price = 10.99;
+console.log(price);
+let gpa = 3;
+console.log(gpa);
+console.log(`your age is ${age} years old`);

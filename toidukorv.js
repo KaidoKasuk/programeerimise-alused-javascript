9b127855b463c89e84819920a528275e6931cb65
KaidoKasuk <kaido.kasuk@voco.ee>
2025-11-17 19:15:57 +0200

    toidukorv

--- toidukorv.js
+++ toidukorv.js
@@ -5,14 +5,14 @@ let toidukorv = [
   { name: "milk", quantity: 1 },
   { name: "bread", quantity: 2 },
 ];
-//1
+//1 sum
 const sum = toidukorv.reduce((total, item) => total + item.quantity, 0);
 
 console.log(sum);
-//2
+//2 yogurt
 toidukorv.push({ name: "yogurt", quantity: 3 });
 
-//3 skull.emoji
+//3 apple +3
 
 const newApple = toidukorv.find((item) => item.name === "apple");
 
@@ -21,5 +21,29 @@ if (newApple) {
 } else {
   toidukorv.push({ name: "apple", quantity: 3 });
 }
+//4 remove banana
+
+const banana = toidukorv.find((item) => item.name === "banana");
+
+if (banana) {
+  toidukorv.splice(toidukorv.indexOf(banana), 1);
+} else {
+}
+
+//5 only names
+const names = toidukorv.map((item) => item.name);
+console.log(names);
+
+//milk?
+
+const milk = toidukorv.find((item) => item.name === "milk");
+if (milk) {
+  milkInArray = true;
+} else {
+  milkInArray = false;
+}
+console.log(`Is milk inside array: ` + milkInArray);
+//sorting
+toidukorv.sort((b, a) => a.quantity - b.quantity);
 
 console.log(toidukorv);
